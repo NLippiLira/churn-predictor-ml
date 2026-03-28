7f2bf4d377c52bccf284f4f217ceadb765e51583
NLIPPI <nlippilira@gmail.com>
2026-03-28 19:50:09 -0300

    deploy

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ form.addEventListener("submit", async (e) => {
     };
 
     try {
-        const response = await fetch("http://127.0.0.1:5000/predict", {
+        const response = await fetch("https://churn-predictor-ml.onrender.com/predict", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
